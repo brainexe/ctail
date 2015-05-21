d28c2b9d97e42345d5b1afa87d86b4a872734591
Matze <matze@mdoetsch.de>
2015-05-21 15:32:34 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,32 @@
+ctail
+=====
+
+Make your tail mor colorful!
+
+Install
+=======
+
+```
+npm install -g ctail
+```
+
+Usage
+=====
+```
+ctail logs/*
+ctail /var/log/messages /var/log/syslog
+ctail /var/log/messages --date
+```
+
+Options
+=======
+```
+Usage: ctail <files>... [options]
+
+files     File(s) to watch at
+
+Options:
+   -d, --date      Prefix all lines with current time
+   -v, --verbose   Print debugging info
+   -c, --color     Colorize the output per file  [true]
+```
---
+++ ctail.js
@@ -0,0 +1,77 @@
+var
+    Tail = require('tail').Tail,
+    glob = require("glob"),
+    colors = require('colors'),
+    nomnom = require("nomnom");
+
+console.log(colors);
+
+var opts = nomnom
+    .option('files', {
+        required: true,
+        position: 0,
+        list: true,
+        help: 'File(s) to watch at'
+    })
+    .option('date', {
+        abbr: 'd',
+        flag: true,
+        help: 'Prefix all lines with current time'
+    })
+    .option('verbose', {
+        abbr: 'v',
+        flag: true,
+        help: 'Print debugging info'
+    })
+    .option('color', {
+        flag: true,
+        abbr: 'c',
+        default: 'true',
+        help: 'Colorize the output per file'
+    })
+    .parse();
+
+var files    = '{' + opts.files.join(',') + '}';
+var lastFile = '';
+var colorIdx = 0;
+var availableColors = [
+    'grey', 'yellow', 'red', 'green', 'blue', 'white', 'cyan', 'magenta'
+];
+
+glob(files, {}, function(error, files) {
+    console.log(error);
+    console.log(files);
+    files.forEach(function(file) {
+
+        var color = availableColors[colorIdx++ % availableColors.length];
+        if (opts.verbose) {
+            console.log(colors[color]("Watch for " + file));
+        }
+
+        var tail = new Tail(file);
+
+        tail.on("line", function (data) {
+            if (lastFile != file) {
+                console.log('==> ' + colors[color](file) + ' <==');
+                lastFile = file;
+            }
+
+            if (opts.date) {
+                data = getDate() + ': ' + data;
+            }
+
+            console.log(colors[color](data));
+        });
+
+        tail.on("error", function (error) {
+            console.log('ERROR: ', error.red);
+        });
+    });
+});
+
+function getDate() {
+    return new Date()
+        .toISOString()
+        .replace(/T/, ' ')
+        .replace(/\..+/, '');
+}
