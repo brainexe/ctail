7f8732b6bec0a0b726408eb2427731a369329475
matze <matze@mdoetsch.de>
2015-05-22 18:25:13 +0200

    added option --theme=rainbow

--- README.md
+++ README.md
@@ -20,7 +20,7 @@ ctail /var/log/messages /var/log/syslog
 ctail /var/log/messages --date
 ```
 
-![Example](https://space.mdoetsch.de/index.php/s/JXjauX3O4qls9kQ/download)
+![Example](https://space.mdoetsch.de/index.php/apps/files_sharing/ajax/publicpreview.php?x=1307&y=790&a=true&file=ctail.png&t=JXjauX3O4qls9kQ&scalingup=0)
 
 
 Options
@@ -33,7 +33,6 @@ files     File(s) to watch at
 Options:
    -d, --date      Prefix all lines with current time
    -v, --verbose   Print debugging info
-   -c, --color     Colorize the output per file  [true]
 ```
 
 Tests
--- ctail.js
+++ ctail.js
@@ -1,9 +1,7 @@
 #!/usr/bin/env node
 
 var
-    Tail   = require('tail').Tail,
-    glob   = require("glob"),
-    colors = require('colors'),
+    CTail   = require('./lib/ctail'),
     nomnom = require("nomnom");
 
 var opts = nomnom
@@ -23,57 +21,11 @@ var opts = nomnom
         flag: true,
         help: 'Print debugging info'
     })
-    .option('color', {
-        flag: true,
-        abbr: 'c',
-        default: 'true',
-        help: 'Colorize the output per file'
+    .option('style', {
+        list: ['rainbow', 'zebra', 'america', 'random', 'trap'],
+        help: 'Color style'
     })
     .parse();
 
-var files    = '{' + opts.files.join(',') + '}';
-var lastFile = '';
-var colorIdx = 0;
-var availableColors = [
-    'grey', 'yellow', 'red', 'green', 'blue', 'white', 'cyan', 'magenta'
-];
-
-glob(files, function(error, files) {
-    if (0 === files.length) {
-        console.error(colors.red('No files matched your pattern'));
-        return;
-    }
-
-    files.forEach(function(file) {
-        var color = colors[availableColors[colorIdx++ % availableColors.length]];
-        if (opts.verbose) {
-            console.log(color("Watch for " + file));
-        }
-
-        var tail = new Tail(file);
-
-        tail.on("line", function (data) {
-            if (lastFile != file) {
-                console.log('==> ' + color(file) + ' <==');
-                lastFile = file;
-            }
-
-            if (opts.date) {
-                data = getDate() + ': ' + data;
-            }
-
-            console.log(color(data));
-        });
-
-        tail.on("error", function (error) {
-            console.log('ERROR: ', colors.red(error));
-        });
-    });
-});
-
-function getDate() {
-    return new Date()
-        .toISOString()
-        .replace(/T/, ' ')
-        .replace(/\..+/, '');
-}
+var ctail = new CTail(opts);
+ctail.tail();
